eb830cdc7dc3c8a8cbbf8dad1a3cf10e901dd6fd
gk <glebk@sibdc.ru>
2018-10-03 16:06:07 +0700

    Using telnetlib to connect

--- locate.py
+++ locate.py
@@ -1,13 +1,29 @@
 from telnetlib import Telnet
+#from netmiko import ConnectHandler
 
 h,l,p="sw3115","locator","good&flow!"
+COMMAND="dis clock"
 
-try:
-    tn = Telnet(h)
-except:
-    print "Connection Failed"
+with telnetlib.Telnet(h) as t:
+    t.read_until(b'Username:')
+    t.write(l + b'\n')
 
+    t.read_until(b'Password:')
+    t.write(p + b'\n')
+#    t.write(b'enable\n')
 
-tn.write(l+"\n")#this is the username
+#    t.read_until(b'Password:')
+#    t.write(ENABLE_PASS + b'\n')
+#    t.write(b'terminal length 0\n')
+    t.write(COMMAND + b'\n')
 
-tn.write(p+"\n")#this is the password
+    time.sleep(5)
+
+    output = t.read_very_eager().decode('utf-8')
+    print(output)
+    
+## old Connection
+
+#tn.write(l+"\n")#this is the username
+
+#tn.write(p+"\n")#this is the password
