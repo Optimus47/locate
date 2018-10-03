e28e50bbbe62a461a8dad9219b3da06ea8f9762c
gk <glebk@sibdc.ru>
2018-10-03 16:15:49 +0700

    Looks to work)

--- locate.py
+++ locate.py
@@ -1,27 +1,29 @@
-from telnetlib import Telnet
+import telnetlib
 #from netmiko import ConnectHandler
 
 h,l,p="sw3115","locator","good&flow!"
 COMMAND="dis clock"
 
-with telnetlib.Telnet(h) as t:
-    t.read_until(b'Username:')
-    t.write(l + b'\n')
+t = telnetlib.Telnet(h)
+t.read_until(b'Username:')
+t.write(l + b'\n')
 
-    t.read_until(b'Password:')
-    t.write(p + b'\n')
+t.read_until(b'Password:')
+t.write(p + b'\n')
 #    t.write(b'enable\n')
 
 #    t.read_until(b'Password:')
 #    t.write(ENABLE_PASS + b'\n')
 #    t.write(b'terminal length 0\n')
-    t.write(COMMAND + b'\n')
+t.write(COMMAND + b'\n')
+t.write(b'quit\n')
+print t.read_all()
 
-    time.sleep(5)
+#    time.sleep(5)
 
-    output = t.read_very_eager().decode('utf-8')
-    print(output)
-    
+#    output = t.read_very_eager().decode('utf-8')
+#    print(output)
+
 ## old Connection
 
 #tn.write(l+"\n")#this is the username
